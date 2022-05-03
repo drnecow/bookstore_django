5708fd88423e5c65ffed2df70dfcd8504a69ce42
drnecow <drnecow@gmail.com>
2022-05-04 00:56:22 +0600

    Add JWT authentication
    Imported Django Rest Framework along with SimpleJWT and added authentication through it.

--- Bookstore/Bookstore/urls.py
+++ Bookstore/Bookstore/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Paths to obtain, refresh and verify JSON web token for JWT authorization
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
